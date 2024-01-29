21116bf2cb3aeef68d0e30d891a7269ed6e3f593
Serhii Revva <besha2vox@gmail.com>
2024-01-30 00:08:11 +0200

    formated hw#7

--- hw7/index.php
+++ hw7/index.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
-Написати програму на PHP, яка містить іменовану функцію, що приймає три аргументи:
-два обовʼязкових типу int і третій необовʼязковий типу closure
-Функція має повертати результат множення першого і другого аргументів, а в разі
-передачі третього аргументу (анонімної функції), перед return має викликати
-анонімну функції і передати в неї результат обчислення.
-Реалізувати аноннімну функцію, яка виводить на екран переданий аргумент.
- */
-
 function  multiplication(int|float $number1, int|float $number2, ?closure $func = null): int|float
 {
     $result = $number1 * $number2;
@@ -19,8 +10,6 @@ function  multiplication(int|float $number1, int|float $number2, ?closure $func 
 
     return $result;
 }
-
-
 
 $result1 = multiplication(12, 13.4);
 echo "Resalt wihout callback: $result1" . PHP_EOL;
